7f501f310c59df2c6214ca5efb874b4cce1ecb3c
Levi Borland <borland.levi@gmail.com>
2019-08-15 07:23:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(time_in_seconds)
+  n = time_in_seconds
+  while n > -1 do
+    puts "#{n}SECOND(S)!"
+    n -= 1 
+  end
+  puts "HAPPY NEW YEAR!"
 end
